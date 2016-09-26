9b55e3fbb52d8c69605be94789edf27abd6c507a
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2016-09-26 12:54:39 -0600

    script cluster up and down wip

--- build.py
+++ build.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
-import os, getopt, sys, subprocess
+import os, getopt, sys, subprocess, json
 
 IMAGE_NAME = "test_mariadb"
 OFFICIAL_IMAGE_NAME = "mariadb:latest"
 PASSWORD = "pass"
+
+NODE_0 = "mariadb_node_0"
+NODE_1 = "mariadb_node_1"
+NODE_2 = "mariadb_node_2"
 
 def build_image():
     subprocess.call(["docker", "build", "-t", IMAGE_NAME, "."])
@@ -20,9 +24,22 @@ def add_node():
     # Add node to default cluster
     subprocess.call(["docker", "run", "--network=bridge", "-d", "-p", "10667:3306", IMAGE_NAME, "mysqld","--wsrep_cluster_address=gcomm://172.17.0.2:3306"]) 
 
-def build_test_cluster():
-    subprocess.call(["docker", "run", "--name=maria_cluster_host" ,"--network=bridge", "-d", "-p", "10666:3306", IMAGE_NAME, "mysqld","--wsrep-new-cluster"]) 
-    subprocess.call(["docker", "run", "--network=bridge", "-d", "-p", "10667:3306", IMAGE_NAME, "mysqld","--wsrep_cluster_address=gcomm://maria_cluster_host:3306"]) 
+def start_test_cluster():
+    # Start cluster
+    subprocess.call(["docker", "run", "--name="+NODE_0 ,"--network=bridge", "-d", "-p", "10666:3306", IMAGE_NAME, "mysqld","--wsrep-new-cluster"]) 
+    #docker inspect mariadb_node_0 | grep IPAddress | cut -d '"' -f 4
+    # Get ip of first node
+    ps = subprocess.Popen(("docker", "inspect", NODE_0), stdout=subprocess.PIPE)
+    out, err = ps.communicate()
+
+    ins = json.loads(out)
+    ip_address = ins[0]['NetworkSettings']['IPAddress']
+    subprocess.call(["docker", "run", "--name="+NODE_1 ,"--network=bridge", "-d", "-p", "10667:3306", IMAGE_NAME, "mysqld","--wsrep_cluster_address=gcomm://"+ip_address]) 
+    subprocess.call(["docker", "run", "--name="+NODE_2 ,"--network=bridge", "-d", "-p", "10668:3306", IMAGE_NAME, "mysqld","--wsrep_cluster_address=gcomm://"+ip_address]) 
+
+def stop_test_cluster():
+    subprocess.call(["docker", "stop", NODE_0, NODE_1, NODE_2])
+    subprocess.call(["docker", "rm", NODE_0, NODE_1, NODE_2])
 
 def usage():
     print "usage"
@@ -30,7 +47,7 @@ def usage():
 if __name__ == '__main__':
     # Get opts
     try:
-        long_args = ["help", "image", "start_cluster", "add_node", "build_default"]
+        long_args = ["help", "image", "start-cluster", "stop-cluster", "add-node", "build-default"]
         opts, args = getopt.getopt(sys.argv[1:], "be:he:s:c:r:", long_args)
         print "Opts : ", opts
         print "Args : ", args
@@ -47,7 +64,7 @@ if __name__ == '__main__':
                     build_image()
                 if a in ("delete"):
                     delete_image()
-        if o in ("--start_cluster"):
-            start_cluster()
-        if o in ("--build_default"):
-            build_test_cluster()
+        if o in ("--start-cluster"):
+            start_test_cluster()
+        if o in ("--stop-cluster"):
+            stop_test_cluster()
