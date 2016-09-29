66b6a618b378b67d0ce5c4bb1a2661fc6d2ed4f4
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2016-09-28 18:09:37 -0600

    add if not exists

--- mariadb/setup.sql
+++ mariadb/setup.sql
@@ -3,8 +3,8 @@
  * Create users to mysql.user table 
  * Create HAPROXY users, you should probably specificy the actual host.
  */
-CREATE USER 'haproxy_check'@'%' IDENTIFIED BY 'password';
-CREATE USER 'haproxy_root'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS 'haproxy_check'@'%' IDENTIFIED BY 'password';
+CREATE USER IF NOT EXISTS 'haproxy_root'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON *.* TO 'haproxy_root'@'%';
 FLUSH PRIVILEGES; -- reload user table
 
