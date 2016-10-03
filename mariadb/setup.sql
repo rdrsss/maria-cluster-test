8d3599d74858d1c513bbdd464c3cb4bb21c87c4b
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2016-10-03 14:50:41 -0600

    add test user

--- mariadb/setup.sql
+++ mariadb/setup.sql
@@ -5,5 +5,10 @@
  */
 CREATE USER IF NOT EXISTS 'maxscale'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON *.* TO 'maxscale'@'%';
+
+CREATE USER IF NOT EXISTS 'test_user'@'%' IDENTIFIED BY 'beefcake';
+GRANT ALL PRIVILEGES ON *.* TO 'test_user'@'%';
+
 FLUSH PRIVILEGES; -- reload user table
+
 
