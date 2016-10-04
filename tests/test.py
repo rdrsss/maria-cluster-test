0cf94733d3952638aeda2202e944fa1c77b20f23
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2016-10-04 13:59:48 -0600

    initial commit for tests

---
+++ tests/test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#
+# @filename : test.py
+# @author   : Manuel A. Rodriguez (manuel.rdrs@gmail.com)
+# @breif    : Test script to access a given cluster and test it.
+import os, getopt, sys
+
+
+def Usage():
+    print "usage"
+
+# Main
+if __name__=='__main__':
+    # Get opts
+    try:
+        long_args = [
+                "help",
+                "run"
+                ]
+        opts, args = getopt.getopt(sys.argv[1:], "be:he:s:c:r:", long_args)
+        print "Opts : ", opts
+        print "Args : ", args
+    except getopt.GetoptError as err:
+        print "opt err: ", str(err)
+        Usage()
+        sys.exit(2)
+
+
